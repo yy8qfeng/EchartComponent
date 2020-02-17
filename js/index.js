c7211fc101fc135b910c1f42841d13b9f5d50f33
LinLianGen <158510825151@qq.com>
2020-02-17 16:54:34 +0800

    提交人：林连根 提交时间：2020年2月17日16:54:19 提交内容：图表代码整理

--- js/index.js
+++ js/index.js
@@ -2,11 +2,23 @@ $(function(){
 
 	  let data={
 		/*	 */
-		 legend:["a","b"],
-		 type:"bar"
+		/* mapData:[{ "name":"上海", "value":100},
+				 { "name":"宁夏", "value":88},
+				 { "name":"湖南", "value":50},
+				 { "name":"湖北", "value":120},
+				 { "name":"江苏", "value":160}]
+		 */
+		/* legend:["a","b"],
+		 type:"bar" */
 		  /*  num: [5, 20, 36, 10, 10, 20],
 		  title:"入门", */
+		  fontColor:'#ffffff'
 	  }; 
 		//oneEchartsT( document.getElementById("oneEcharts") , data);
-	  getCharTypeDoubleBar( document.getElementById("oneEcharts") , data);
+		//getCharTypeDoubleBar( document.getElementById("oneEcharts") , data);
+		//getCharByChinaMap(document.getElementById("oneEcharts") , data);	
+		//getCharByRadar(document.getElementById("oneEcharts") , data);
+		//getEcharPictorialBar(document.getElementById("oneEcharts") , data);
+		//getTopMap(document.getElementById("oneEcharts") , data);
+		getEchartsTimePie(document.getElementById("oneEcharts"),data)
 });
